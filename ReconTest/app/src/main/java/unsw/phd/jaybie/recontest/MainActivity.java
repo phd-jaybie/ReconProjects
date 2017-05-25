a85453721b146cd57d4ea11c935b00812d4ebabe
Jaybie A. de Guzman <j.deguzman@student.unsw.edu.au>
2017-05-25 15:09:29 +1000

    Simple Recon App with a single item in a list, i.e. A camera intent.

--- ReconTest/app/src/main/java/unsw/phd/jaybie/recontest/MainActivity.java
+++ ReconTest/app/src/main/java/unsw/phd/jaybie/recontest/MainActivity.java
@@ -1,13 +1,34 @@
 package unsw.phd.jaybie.recontest;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import com.reconinstruments.ui.list.SimpleListActivity;
+import com.reconinstruments.ui.list.StandardListItem;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends SimpleListActivity {
+
+    static final String CAMERA_APP= "com.reconinstruments.camera";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_standard_layout);
+        setContents(
+            new ListItem("Start Camera", new Intent(CAMERA_APP)));//,
+            //new ListItem("Start Activity B", IntentB),
+            //new ListItem("Start Activity C", IntentC));
+    }
+
+    public class ListItem extends StandardListItem {
+        Intent intent;
+        public ListItem(String text, Intent i) {
+            super(text);
+            this.intent = i; }
+        public void onClick(Context context) {
+            context.startActivity(intent);
+        }
     }
 }
